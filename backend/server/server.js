9005ae77643014a675c7a20f07d3d235d71bf2e9
Ian <ianswensson@gmail.com>
2021-12-08 11:10:01 -0700

    Server setup

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,8 +2,11 @@ const path = require('path');
 const express = require('express');
 const axios = require('axios');
 
+// instantiate app
 const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(6969, console.log('Connected to the Arctic'));
+app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, '../../client/dist')));
+
+app.listen(3000, console.log('Connected to the Arctic'));
