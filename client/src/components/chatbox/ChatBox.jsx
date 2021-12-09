7b3370e17d5840a90c8976cd688f9aba265f8730
Wilson Wong <wilson.ka.wong@gmail.com>
2021-12-09 16:32:51 -0600

    Added send message functionality

--- client/src/components/chatbox/ChatBox.jsx
+++ client/src/components/chatbox/ChatBox.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-cycle */
-
 import axios from 'axios';
 import React, { useState, useContext, useEffect } from 'react';
 import { getUserName } from './chatBoxHelperFunctions.js';
@@ -14,6 +13,12 @@ export default function ChatBox({ senderID, recipientID }) {
   const [messageText, setMessageText] = useState('');
 
   const handleChatBoxClick = () => { setChatBoxActive(!chatBoxActive); };
+  const getMessageHistory = () => {
+    axios.get('http://localhost:3000/messages', {})
+      .then((results) => { console.log('GOT MESSAGES BACK:', results.data); })
+      .then(() => { setMessageHistory([]); })
+      .catch();
+  };
   const handleSendMessage = () => {
     axios.post('http://localhost:3000/messages', {
       sender_user_id: senderID,
@@ -21,14 +26,10 @@ export default function ChatBox({ senderID, recipientID }) {
       text: messageText,
       dateTime: new Date()
     })
-      .then()
+      .then(() => { getMessageHistory(); })
+      .then(() => { setMessageText(''); })
+      // .then(() => { console.log('Message submitted'); })
       .catch((err) => { console.log(err); });
-  };
-  const getMessageHistory = () => {
-    axios.get('http://localhost:3000/messages', {})
-      .then((results) => { console.log('GOT MESSAGES BACK:', results); })
-      .then(() => { setMessageHistory([]); })
-      .catch();
   };
 
   useEffect(() => { getMessageHistory(); }, []);
@@ -41,7 +42,6 @@ export default function ChatBox({ senderID, recipientID }) {
       {chatBoxActive
         ? (<div>
             <div>
-              {/* TODO: Show message history here */}
               {messsageHistory.map((message) => { return <MessageTile message={message} />; })}
               <div className="messageInputBox">
                 <input type="text" className="messageInput" onChange={(e) => { setMessageText(e.nativeEvent.target.value); }}></input>
