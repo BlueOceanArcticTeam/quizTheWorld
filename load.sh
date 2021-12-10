acf3da076fa9c3ac5dfc7261967e224548a4c808
Matthew Olsen <mbolsen@gmail.com>
2021-12-09 17:00:32 -0700

    Almost finished with load.sh to load the data

---
+++ load.sh
@@ -0,0 +1,9 @@
+\i backend/database/schema.sql
+
+INSERT INTO users (id, username, password) VALUES (1,'QuizKnows', 'iamcool');
+
+\COPY quizzes(id, category, difficulty, source, datecreated, numsubmissions) FROM './backend/database/data/quizzes.csv' DELIMITER ',' CSV HEADER;
+
+\COPY questions(id, quiz_id, text, thumbnail_url, questionType, learnmore_url) FROM './backend/database/data/question.csv' DELIMITER ',' CSV HEADER;
+
+\COPY answers(id, correct, text) FROM './backend/database/data/answers.csv' DELIMITER ',' CSV HEADER;
