0af44e304d9793f52a5803ebabff99affb147640
Wilson Wong <wilson.ka.wong@gmail.com>
2021-12-10 19:54:29 -0600

    Started writing user route

--- backend/server/routes/messageRoute.js
+++ backend/server/routes/messageRoute.js
@@ -2,21 +2,22 @@
 /* eslint-disable import/extensions */
 const express = require('express');
 
-const messageRoutes = express.Router();
+const messageRoute = express.Router();
 const db = require('../../database/db.js');
 
-messageRoutes.route('/').get((req, res) => {
+messageRoute.route('/').get((req, res) => {
   db.query('SELECT * FROM messages', [], (err, data) => {
     if (err) throw err;
     res.send(data.fields);
   });
 });
 
-messageRoutes.route('/history').get((req, res) => {
+messageRoute.route('/history').get((req, res) => {
   const { senderID, recipientID } = req.body;
   db.query(
     // `SELECT * FROM messages
     //   WHERE (sender_user_id=$1 OR sender_user_id=$2) AND (recipient_user_id=$1 OR recipient_user_id=$2)`,
+    // TODO: Refactor query to be dynamic
     `SELECT * FROM messages
     WHERE (sender_user_id=1 OR sender_user_id=2) AND (recipient_user_id=1 OR recipient_user_id=2)`,
     // [senderID, recipientID],
@@ -27,7 +28,7 @@ messageRoutes.route('/history').get((req, res) => {
   );
 });
 
-messageRoutes.route('/').post((req, res) => {
+messageRoute.route('/').post((req, res) => {
   const { senderID, recipientID, text, date } = req.body;
   db.query(
     'INSERT INTO messages (sender_user_id, recipient_user_id, text, date) VALUES ($1, $2, $3, $4)',
@@ -39,4 +40,4 @@ messageRoutes.route('/').post((req, res) => {
   );
 });
 
-module.exports = messageRoutes; // CHANGE 'TEMPLATE' TO YOUR ROUTE
+module.exports = messageRoute; // CHANGE 'TEMPLATE' TO YOUR ROUTE
---
+++ backend/server/routes/userRoute.js
@@ -0,0 +1,6 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable import/extensions */
+const express = require('express');
+
+const userRoute = express.Router();
+const db = require('../../database/db.js');
