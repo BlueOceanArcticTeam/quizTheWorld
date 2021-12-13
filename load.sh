d224d3e350e4fd6fb2d4521e0ebf3ca510df3476
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-12-13 16:45:46 -0700

    Quizzes are takeable. Keep an eye out for bugs

--- load.sh
+++ load.sh
@@ -2,9 +2,9 @@
 
 \c quizknows
 
-INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (1,'QuizKnows', 'iamcool', 'Quiz', 'Knows', 'null', 'empty@empty.com');
-INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (2,'Gerald', 'iamcool', 'Gerald', 'Anderson', 'null', 'gerald@anderson.com');
-INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (3,'SomoneElse', 'iamcool', 'Tanya', 'Turkey', 'null', 'tanyat@emailprovider.com');
+INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (DEFAULT,'QuizKnows', 'iamcool', 'Quiz', 'Knows', 'null', 'empty@empty.com');
+INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (DEFAULT,'Gerald', 'iamcool', 'Gerald', 'Anderson', 'null', 'gerald@anderson.com');
+INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, email) VALUES (DEFAULT,'SomoneElse', 'iamcool', 'Tanya', 'Turkey', 'null', 'tanyat@emailprovider.com');
 
 \COPY quizzes(id,title, category, difficulty, source, datecreated, numsubmissions) FROM './backend/database/data/quizzes.csv' DELIMITER ',' CSV HEADER;
 
@@ -12,5 +12,5 @@ INSERT INTO users (id, username, password, firstname, lastname, thumbnail_url, e
 
 \COPY answers(question_id, correct, text) FROM './backend/database/data/answers.csv' DELIMITER ',' CSV HEADER;
 
-INSERT INTO userQuizStatus (id, quiz_id, user_id, completed, dateCompleted, lastAnswered, numCorrect, totalQuestions) VALUES (1, 1, 2, true, '05 Dec 2021', 1, 9, 12);
-INSERT INTO userQuizStatus (id, quiz_id, user_id, completed, dateCompleted, lastAnswered, numCorrect, totalQuestions) VALUES (2, 2, 2, true, '06 Dec 2021', 2, 11, 14);
+INSERT INTO userQuizStatus (id, quiz_id, user_id, completed, dateCompleted, lastAnswered, numCorrect, totalQuestions) VALUES (DEFAULT, 1, 2, true, '05 Dec 2021', 1, 9, 12);
+INSERT INTO userQuizStatus (id, quiz_id, user_id, completed, dateCompleted, lastAnswered, numCorrect, totalQuestions) VALUES (DEFAULT, 2, 2, true, '06 Dec 2021', 2, 11, 14);
