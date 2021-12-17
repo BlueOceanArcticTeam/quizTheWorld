3851dcd7c35269fd5fda2aba24f96b83146ce21b
Jack Dowling <jackdowling@Jacks-MacBook-Pro.local>
2021-12-17 10:00:12 -0700

    trying pool

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,6 +3,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   user: process.env.USERNAME,
   host: 'localhost',
   database: 'quizknows',
