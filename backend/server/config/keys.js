142f12ab027357370af9f2d49421b82222766f21
Matthew Olsen <mbolsen@gmail.com>
2021-12-22 08:41:49 -0700

    removed the keys from the keys file

--- backend/server/config/keys.js
+++ backend/server/config/keys.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   google: {
-    clientID: '932053775960-i730cel287b5qklukr72pl405ljscf9p.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: process.env.GOOGLE_API_KEY,
+    clientSecret: process.send.CLIENTSECRET,
   },
   session: {
-    cookieKey: 'mattisthecoolestpersonintheworld', // this is just a random string that will help encrypt
+    cookieKey: process.env.cookieKey, // this is just a random string that will help encrypt
   },
 };
